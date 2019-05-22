87d57769c24dcb3d2a226ca276d31d76395ef355
Yeni <yen.diaz@hotmail.com>
2019-05-22 06:52:25 -0400

    first commit

---
+++ RegistrationForm/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using RegistrationForm.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RegistrationForm.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Registration()
+        {
+                                        
+            return View();
+        }
+        public ActionResult Result(Person person)
+        {
+            ViewBag.UserName = person.UserName;
+            ViewBag.Email = person.Email;
+            ViewBag.Password = person.Password;
+
+            return View();
+           
+        }
+    }
+}
---
+++ RegistrationForm/Models/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RegistrationForm.Models
+{
+    public class Person
+    {
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
